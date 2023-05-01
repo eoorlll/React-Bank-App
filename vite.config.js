8920c04caef172047b91337be5c2d8a263e0c005
Evgenii Podrezenko <49723859+eoorlll@users.noreply.github.com>
2023-05-01 17:31:52 +0400

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_API_BASE_URL,
+  base: "/React-Bank-App/",
   plugins: [react()],
 });
